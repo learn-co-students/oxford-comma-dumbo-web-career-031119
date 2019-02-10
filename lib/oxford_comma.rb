476853018634ad571b411bdcd30d7313c7b8f6d1
github username <itsDario1@gmail.com>
2019-02-10 11:36:44 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+    if array.length < 3
+        return array.join(" and ")
+    end
+    answer = array.join(", ")
+    answer = answer.reverse.sub(",", "dna ,").reverse
 end

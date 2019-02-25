82894459084bb626b37dfa28a42ae2fba4008298
Jordan R <jordan.rivera34@gmail.com>
2019-02-25 01:11:10 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_ele = "and #{array[-1]}"
+    array.pop
+    array << new_ele
+    array.join(", ")
+  end
 end
